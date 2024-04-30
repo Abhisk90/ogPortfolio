68ea569fe1023ce4fb90583e3976d055a9a23bd1
Abhisk90 <captainjack9021@gmail.com>
2024-04-30 23:23:10 +0530

    newchanges

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -29,13 +29,11 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-  <a className="text-2xl md:text-5xl text-white font-semibold">
-    <Image src={logoImage} alt="Logo" width={100} height={50} /> {/* Adjust width and height as needed */}
-  </a>        </Link>
+      <Link href="/" passHref legacyBehavior>
+    <a className="text-2xl md:text-5xl text-white font-semibold">
+      <Image src={logoImage} alt="Logo" width={100} height={50} />
+    </a>      
+  </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
